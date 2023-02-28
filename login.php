6a7d1d34bd81317af4fd0d604c136ada4b110029
amranhakim10 <amranhakim9@gmail.com>
2023-02-28 21:47:48 +0700

    Initial commit

--- login.php
+++ login.php
@@ -13,16 +13,22 @@ include "include/login/head.php";
         <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
         <form action="execute.php" method="POST">
           <div class="form-group position-relative has-icon-left mb-4">
-            <input type="text" class="form-control form-control-xl" name="username" placeholder="Username">
+            <input type="text" class="form-control form-control-xl" id="username" name="username" placeholder="Username">
             <div class="form-control-icon">
               <i class="bi bi-person"></i>
             </div>
           </div>
           <div class="form-group position-relative has-icon-left mb-4">
-            <input type="password" class="form-control form-control-xl" name="password" placeholder="Password">
+            <input type="password" class="form-control form-control-xl" id="password" name="password" placeholder="Password">
             <div class="form-control-icon">
               <i class="bi bi-shield-lock"></i>
             </div>
+          </div>
+          <div class="form-check form-check-lg d-flex align-items-end">
+            <input class="form-check-input me-2" type="checkbox" value="" id="ckb1" onclick="myFunction()">
+            <label class="form-check-label text-gray-600" for="ckb1">
+              Show Password
+            </label>
           </div>
           <div class="form-check form-check-lg d-flex align-items-end">
             <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
@@ -32,12 +38,6 @@ include "include/login/head.php";
           </div>
           <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
         </form>
-        <div class="text-center mt-5 text-lg fs-4">
-          <p class="text-gray-600">Don't have an account?
-            <a href="auth-register.html" class="font-bold">Sign up</a>.
-          </p>
-          <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p>
-        </div>
       </div>
     </div>
     <div class="col-lg-7 d-none d-lg-block">
@@ -49,3 +49,15 @@ include "include/login/head.php";
 <?php
 include "include/login/footer.php";
 ?>
+<!-- Show Password -->
+<script type="text/javascript">
+  function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+</script>
+<!-- End Show Password -->
